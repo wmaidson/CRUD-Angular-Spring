018c4d14fc3207f13aff87f53d3873fa728313f6
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-04 23:23:01 -0300

    create course controller

---
+++ crud-spring/src/main/java/com/spring/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.spring.crudspring.controller;
+
+import java.util.List;
+
+import com.spring.crudspring.model.Course;
+import com.spring.crudspring.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.AllArgsConstructor;
+
+@RestControllerAdvice
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+    
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    } 
+}
