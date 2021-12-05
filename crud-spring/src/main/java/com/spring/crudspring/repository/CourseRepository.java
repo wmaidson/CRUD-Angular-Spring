6a2edc00bc093d9b7779901aac56e28c02585b51
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-04 23:23:47 -0300

    create course repository

---
+++ crud-spring/src/main/java/com/spring/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.spring.crudspring.repository;
+
+import com.spring.crudspring.model.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
