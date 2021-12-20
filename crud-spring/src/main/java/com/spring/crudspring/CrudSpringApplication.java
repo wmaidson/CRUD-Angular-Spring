d2b9eda525dbf8388d25da2e72784a3bc1ab200d
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-19 23:12:08 -0300

    aplication modified

--- crud-spring/src/main/java/com/spring/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/spring/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.spring.crudspring;
 
+import com.spring.crudspring.model.Course;
+import com.spring.crudspring.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,15 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner initDataBase(CourseRepository courseRepository) {
+        return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+        };
+    }
 }
